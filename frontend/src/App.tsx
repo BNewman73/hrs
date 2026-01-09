77e7b0aeebb5c7b36ea8e7f66143505a15804b7a
BNewman73 <bcnewman73@gmail.com>
2026-01-09 10:38:26 -0600

    cleaning up booking guest views

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,11 @@ import GlobalSnackbar from "./components/GlobalSnackbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DashboardPage from "./components/DashboardPage";
 import LoginPage from "./components/login/LoginPage";
-import RoomAvailability from "./components/reservations/RoomAvailability";
 import RoomTypesPage from "./components/reservations/RoomTypesPage";
 import BookingCancel from "./components/reservations/BookingCancel";
 import BookingSuccess from "./components/reservations/BookingSuccess";
 import HomePage from "./components/HomePage";
+import BookingPage from "./components/reservations/BookingPage";
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
         <Routes>
           <Route path="/Login" element={<LoginPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/rooms/:roomType" element={<RoomAvailability />} />
+          <Route path="/rooms/:roomType" element={<BookingPage />} />
           <Route path="/types" element={<RoomTypesPage />} />
           <Route path="/booking-success" element={<BookingSuccess />} />
           <Route path="/booking-cancel" element={<BookingCancel />} />
